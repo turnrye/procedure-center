92e1804e9c67c9d25710b272217e01f27407c204
Ryan Turner <ryan.e.t@gmail.com>
2016-12-29 15:01:24 -0600

    Added clicktrack and 2 minute timer to CPR page; more work on #7

--- src/pages/bls-cpr-components/bls-cpr-components.ts
+++ src/pages/bls-cpr-components/bls-cpr-components.ts
@@ -7,10 +7,24 @@ import { GoogleAnalytics } from 'ionic-native';
   templateUrl: 'bls-cpr-components.html'
 })
 export class BLSCPRComponentsPage {
+  metronome: HTMLAudioElement;
+  remainingTime: number = 0.0;
 
-  constructor(public navCtrl: NavController) {}
+  constructor(public navCtrl: NavController) {
+    this.metronome = new Audio('assets/100bpm-click-track-2min.mp3');
+    setInterval(() => this.remainingTime = Math.floor(this.metronome.duration - this.metronome.currentTime), 1000);
+    this.metronome.loop = true;
+  }
 
   ionViewDidEnter() {
     GoogleAnalytics.trackView("bls-cpr-components");
+  }
+  toggleMetronome() {
+    if(this.metronome.paused) {
+      this.metronome.currentTime = 0.0;
+      this.metronome.play();
+    } else {
+      this.metronome.pause();
+    }
   }
 }
