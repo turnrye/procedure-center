5f479c7669d1c20f08944f474902ed095ecd9be6
Ryan Turner <ryan.e.t@gmail.com>
2017-01-12 21:54:53 -0600

    Added pain scale help

--- src/pages/faces-pain-scale/faces-pain-scale.ts
+++ src/pages/faces-pain-scale/faces-pain-scale.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, PopoverController } from 'ionic-angular';
+import { GoogleAnalytics } from 'ionic-native';
+import { FacesPainScaleHelpPage } from '../faces-pain-scale-help/faces-pain-scale-help';
 
 @Component({
   selector: 'page-faces-pain-scale',
@@ -7,6 +9,16 @@ import { NavController } from 'ionic-angular';
 })
 export class FacesPainScalePage {
 
-  constructor(public navCtrl: NavController) {}
+  constructor(public navCtrl: NavController, public popoverCtrl: PopoverController) {}
 
+  ionViewDidLoad() {
+    GoogleAnalytics.trackView("faces-pain-scale");
+  }
+
+  presentPopover(myEvent) {
+    let popover = this.popoverCtrl.create(FacesPainScaleHelpPage);
+    popover.present({
+      ev: myEvent
+    });
+  }
 }
